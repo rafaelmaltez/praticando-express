3843b36548d6f7d416dbf6c457127224e9c4ea3f
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-20 17:14:56 -0300

    cria estrutura de pastas

---
+++ middlewares/login.js
@@ -0,0 +1,5 @@
+// Objetivo: Receber uma requisição que envie email/senha e receba um token como resposta. O formato desse token deve ser uma string aleatória com 12 caracteres. O email recebido deve ter o formato email@mail.com e a senha deve conter no mínimo 4 caracteres e no máximo 8, todos números. Caso algum desses campos seja enviado em formato incorreto, deve-se retornar uma mensagem de erro email or password is incorrect. e seu devido status code.
+
+module.exports = (req, res) => {
+
+}
